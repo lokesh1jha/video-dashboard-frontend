de0585717353406ab84e3b855d3f4f8d5ad8c102
lokeshjha <lokesh1jha@gmai.com>
2024-02-28 21:02:39 +0530

    wip: cleaned repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AuthRoute from './Route/AuthRoute.jsx';
 import NonAuthRoute from './Route/NonAuthRoute.jsx';

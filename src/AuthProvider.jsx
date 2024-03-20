8c528073351e068a4a413a6a59807fa3bb929a7d
lokeshjha <lokesh1jha@gmai.com>
2024-03-21 02:54:54 +0530

    wip

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,11 +1,25 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({});
-  
+  useEffect(() => {
+    const token = localStorage.getItem('Authorization');
+    if (!token) {
+      setIsLoggedIn(false)
+    }
+    else {
+      const tokenDecoded = token ? jwtDecode(token) : null;
+      setUser({
+        user_type: tokenDecoded.user_type,
+        is_youtube_authenticated: tokenDecoded.is_youtube_authenticated
+      })
+    }
+    
+  }, [isLoggedIn])
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
